cc886adf36ad0e67dbaae509c8fe9a2d695204bd
maro0226 <maro0226@naver.com>
2021-09-29 13:10:27 +0900

    R 실습

--- Ch06/6_1_dplyr 패키지 활용.R
+++ Ch06/6_1_dplyr 패키지 활용.R
@@ -75,3 +75,96 @@ df_teacher
 
 df_join <- inner_join(df_exam, df_teacher, by='class')
 View(df_join)
+
+
+#교재 p169 실습 - iris 데이터셋을 대상으로 %>% 연산자를 이용하여 함수 적용하기
+iris %>% head()
+iris %>%  head() %>%  subset(Sepal.Length >= 5.0)
+
+
+#교재 p170 실습 - dplyr패키지와 hflight 데이터셋 설치
+install.packages("hflights")
+library(hflights)
+install.packages("dplyr")
+library(dplyr)
+
+#교재 p171 실습 - hflight 데이터셋 구조 보기
+str(hflights)
+
+
+#교재 p171 실습 - tbl_df() 함수 사용하기
+hflights_df <- tbl_df(hflights)
+hflights_df
+
+
+#교재 p172 실습 - hflights_df를 대상으로 특정일의 데이터 추출하기
+filter(hflights_df, Month == 1 & DayofMonth == 2)
+
+
+#교재 p173 실습 - hflights_df를 대상으로 지정된 월의 데이터 추출하기
+filter(hflights_df, Month == 1 | Month == 2)
+
+
+#교재 p174 실습 - hflights_df를 대상으로 데이터 정렬하기
+arrange(hflights_df, Year, Month, DepTime, ArrTime)
+
+
+#교재 p175 실습 - hflights_df를 대상으로 지정된 칼럼 데이터 검색하기
+select(hflights_df, Year, Month, DepTime, ArrTime)
+
+
+#교재 p175 실습 - hflights_df를 대상으로 칼럼의 범위로 검색하기
+select(hflights_df, Year:ArrTime)
+
+#교재 p176 실습 - hflights_df에서 출발 지연시간과 도착 지연시간의 차이를 계산한 컬럼 추가하기
+mutate(hflights_df, gain = ArrDelay - DepDelay,
+       gain_per_hour = gain / (AirTime / 60))
+
+
+#교재 p177 실습 - mutate()함수에 의해 추가된 칼럼 보기
+select(mutate(hflights_df.gain = ArrDelay - DepDelay,
+              gain_per_hour = gain / (AirTime / 60)),
+       Year, Month, ArrDelay, DepDelay, gain, gain_per_hour)
+
+
+#교재 p177 실습 - hflights_df에서 비행시간의 평균 구하기
+summarise(hflights_df, avgAriTime = mean(AirTime, na.rm = TRUE))
+
+
+#교재 p178 실습 - hflights_df의 관측치 길이 구하기
+summarise(hflights_df, cnt = n(), 
+          delay = mean(AirTime, na.rm = TRUE))
+
+
+#교재 p178 실습 - 도착시간의 표준편차와 분산 계산하기
+summarise(hflights_df, arrTimeSd = sd(ArrTime, na.rm = TRUE),
+          arrTimeVar = var(ArrTime, na.rm = T))
+
+
+#교재 p178 실습 - 집단변수를 이용하여 그룹화하기
+species <- group_by(iris, Species)
+str(species)
+species
+
+
+#교재 p180 실습 - 공통변수를 이용하여 내부 조인하기
+df1 <- data.frame(x = 1:5, y = rnorm(5))
+df2 <- data.frame(x = 2:6, z = rnorm(5))
+
+df1
+df2
+
+inner_join(df1, df2, by = 'x')
+
+#교재 p180 실습 - 공통변수를 이용하여 왼쪽 조인하기
+
+#교재 p181 실습 - 공통변수를 이용하여 오른쪽 조인하기
+#교재 p181 실습 - 공통변수를 이용하여 전체 조인하기
+
+#교재 p182 실습 - 두 개의 데이터프레임을 행 단위로 합치기
+
+#교재 p183 실습 - 두 개의 데이터프레임을 열 단위로 합치기
+#교재 p183 실습 - 데이터프레임의 칼럼명 수정하기
+
+
+
--- Ch06/6_2_reshape2 패키지 활용.R
+++ Ch06/6_2_reshape2 패키지 활용.R
@@ -19,3 +19,14 @@ View(melt_result2)
 #dcast - 행을 열로, 세로로 긴 형태의 데이터를 가로로 길게 전환
 dcast_result <- dcast(melt_result1, 번호 ~ variable)
 View(dcast_result)
+
+
+#교재 p184 실습 - reshape2 패키지 설치와 데이터 가져오기
+
+#교재 p185 실습 - 넓은 형식으로 변경하기
+#교재 p185 실습 - 파일 저장 및 읽기
+
+#교재 p186 실습 - 넓은 형식의 데이터를 긴 형식으로 변경하기
+#교재 p186 실습 - smiths 데이터셋 확인하기
+
+#교재 p187 실습 - airquality 데이터 셋의 구조 변경하기
